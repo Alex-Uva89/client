c50d614a7d7f961924bcf651a4b7a4d292b59d8a
Alex-Uva89 <a.uva1988@gmail.com>
2025-01-07 20:22:19 +0100

    Aggiornamento della configurazione del database: impostazione di valori predefiniti per la connessione nel file server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express()
 app.use(cors())
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'menu-mamma-elvira.cpuk68cqabe5.eu-south-1.rds.amazonaws.com',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'adminDev',
+  password: process.env.DB_PASSWORD || '*1qaz2wsx3edc',
+  database: process.env.DB_NAME || 'db_menu'
 })
 
 // Drinks endpoint
