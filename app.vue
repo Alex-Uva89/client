b27f22f72df0aebf43cc19b912ddea93457d5bdd
Alex-Uva89 <a.uva1988@gmail.com>
2025-01-07 21:21:35 +0100

    Aggiornamento della struttura del componente: modifica del nome del componente Navigation per rispettare le convenzioni di denominazione

--- app.vue
+++ app.vue
@@ -2,13 +2,12 @@
   <div class="app">
     <router-view></router-view>
     
-    <navigation />
+    <Navigation />
   </div>
 </template>
 
 <script>
 import defaultLayout from './layouts/homeLayout.vue';
-import navigation from './components/Navigation.vue';
 import Navigation from './components/Navigation.vue';
 
 export default {
